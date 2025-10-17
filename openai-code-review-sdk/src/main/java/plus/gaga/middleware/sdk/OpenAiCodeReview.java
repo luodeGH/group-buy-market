2055e6ba67e2c3d175818d03c6cb1584fe86111a
luogit <hello@luogit.cn>
2025-10-17 21:16:04 +0800

    拼团营销服务+openai代码自动评审v2

---
+++ openai-code-review-sdk/src/main/java/plus/gaga/middleware/sdk/OpenAiCodeReview.java
@@ -0,0 +1,70 @@
+package plus.gaga.middleware.sdk;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import plus.gaga.middleware.sdk.domain.service.impl.OpenAiCodeReviewService;
+import plus.gaga.middleware.sdk.infrastructure.git.GitCommand;
+import plus.gaga.middleware.sdk.infrastructure.openai.IOpenAI;
+import plus.gaga.middleware.sdk.infrastructure.openai.impl.ChatGLM;
+import plus.gaga.middleware.sdk.infrastructure.weixin.WeiXin;
+
+public class OpenAiCodeReview {
+
+    private static final Logger logger = LoggerFactory.getLogger(OpenAiCodeReview.class);
+
+    // 配置配置
+    private String weixin_appid = "wx423a7d9d79a076e4";
+    private String weixin_secret = "fbf2fa15aacf98d0f93d636fe1419766";
+    private String weixin_touser = "oJA_32Ee5KcNHyg8rmADI6NNCXA0";
+    private String weixin_template_id = "WP7DUVPlCPMd6dHKSzKpl2a-QhNKjHWyqf5hdrIql8Q";
+
+    // ChatGLM 配置
+    private String chatglm_apiHost = "https://open.bigmodel.cn/api/paas/v4/chat/completions";
+    private String chatglm_apiKeySecret = "";
+
+    // Github 配置
+    private String github_review_log_uri;
+    private String github_token;
+
+    // 工程配置 - 自动获取
+    private String github_project;
+    private String github_branch;
+    private String github_author;
+
+    public static void main(String[] args) throws Exception {
+        GitCommand gitCommand = new GitCommand(
+                getEnv("GITHUB_REVIEW_LOG_URI"),
+                getEnv("GITHUB_TOKEN"),
+                getEnv("COMMIT_PROJECT"),
+                getEnv("COMMIT_BRANCH"),
+                getEnv("COMMIT_AUTHOR"),
+                getEnv("COMMIT_MESSAGE")
+        );
+
+
+        WeiXin weiXin = new WeiXin(
+                getEnv("WEIXIN_APPID"),
+                getEnv("WEIXIN_SECRET"),
+                getEnv("WEIXIN_TOUSER"),
+                getEnv("WEIXIN_TEMPLATE_ID")
+        );
+
+
+
+        IOpenAI openAI = new ChatGLM(getEnv("CHATGLM_APIHOST"), getEnv("CHATGLM_APIKEYSECRET"));
+
+        OpenAiCodeReviewService openAiCodeReviewService = new OpenAiCodeReviewService(gitCommand, openAI, weiXin);
+        openAiCodeReviewService.exec();
+
+        logger.info("openai-code-review done!");
+    }
+
+    private static String getEnv(String key) {
+        String value = System.getenv(key);
+        if (null == value || value.isEmpty()) {
+            throw new RuntimeException("value is null");
+        }
+        return value;
+    }
+
+}
